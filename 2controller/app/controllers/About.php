dcfe98ab2e3da3a8511a1253ddec81f8829939b8
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2023-03-05 20:56:49 +0700

    Materi controllers

---
+++ 2controller/app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php
+class About
+{
+    public function index($nama = 'Tsaqif', $profesi = 'Pelajar')
+    {
+        echo "Nama saya $nama, saya seorang $profesi";
+    }
+
+    public function page($nama = 'Ovang', $profesi = 'Pelajar')
+    {
+        echo "Nama saya $nama, saya seorang $profesi";
+    }
+}
